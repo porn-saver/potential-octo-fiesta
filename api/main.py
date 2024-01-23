68f9f70d7913a31ea190564aa432a3505dca012a
porn-saver <john.antoon@proton.me>
2024-01-23 21:39:18 +0100

    Fix

--- api/main.py
+++ api/main.py
@@ -1,6 +1,3 @@
-import logging
-import os
-
 from fastapi import FastAPI
 
 import uvicorn
@@ -23,19 +20,5 @@ setup_cors(app)
 
 app.include_router(StarController.router)
 
-# Obtener el directorio temporal del sistema
-temp_dir = '/tmp' if os.path.exists('/tmp') else '/var/task'
-
-# Configura el registro para escribir en la consola y en un archivo en el directorio temporal
-log_file_path = os.path.join(temp_dir, "app.log")
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-    handlers=[
-        logging.StreamHandler(),
-        logging.FileHandler(log_file_path),
-    ]
-)
-logging.info("Deploy")
 if __name__ == "__main__":
     uvicorn.run("api.main:app", host="0.0.0.0", port=int(8080), log_level="info")
