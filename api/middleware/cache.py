02ceb6f2c99e9a5eae24c4c7245f49f8ca1c888b
porn-saver <john.antoon@proton.me>
2024-05-28 17:11:44 +0200

    Fix

--- api/middleware/cache.py
+++ api/middleware/cache.py
@@ -18,24 +18,18 @@ def checkCacheDate(request: Request):
 
 def setCacheData(request: Request, data):
     response = [value.toJson() for value in data]
-    response = response[0] if len(response) == 1 else response
-    aux = copy(request)
-    url: str = str(aux.url).lower().replace(" ", "-")
-    cache_key = f"cached_data:{url}"
-    cache[cache_key] = response
-    return response
+    setCache(response, request)
 
 
 def setCacheDataEnum(request: Request, enum, isJson: bool = False):
     response = enum if isJson else [{data.name: data.value for data in enum}]
+    setCache(response, request)
+
+
+def setCache(response, request):
     response = response[0] if len(response) == 1 else response
     aux = copy(request)
     url: str = str(aux.url).lower().replace(" ", "-")
     cache_key = f"cached_data:{url}"
     cache[cache_key] = response
     return response
-
-
-async def getBody(request: Request):
-    body = []
-    return body
